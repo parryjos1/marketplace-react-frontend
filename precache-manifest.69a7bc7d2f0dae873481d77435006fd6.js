315e9994c7592d17efeab4d9ca3beaffac440762
Josh Parry <josh.parry97@gmail.com>
2019-07-25 18:32:52 +1000

    Updates

---
+++ precache-manifest.69a7bc7d2f0dae873481d77435006fd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d03953dff7ed2e054c6464cc3013ac",
+    "url": "/marketplace-react-frontend/index.html"
+  },
+  {
+    "revision": "5472e6e69ecb95fba357",
+    "url": "/marketplace-react-frontend/static/css/main.d902988d.chunk.css"
+  },
+  {
+    "revision": "e0f590d67ac57d763345",
+    "url": "/marketplace-react-frontend/static/js/2.f0b712eb.chunk.js"
+  },
+  {
+    "revision": "5472e6e69ecb95fba357",
+    "url": "/marketplace-react-frontend/static/js/main.5c81b7b4.chunk.js"
+  },
+  {
+    "revision": "cabbe7dc53c37195852f",
+    "url": "/marketplace-react-frontend/static/js/runtime~main.2f0e10e5.js"
+  }
+]);
